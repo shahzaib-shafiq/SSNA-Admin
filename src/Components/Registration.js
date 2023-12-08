a9d66faae7c207a6788a71887e0abfbcf7378c1d
Shahzaib Shafiq <73356944+shahzaibshafiq100@users.noreply.github.com>
2023-12-08 17:54:01 +0500

    Configuration Added

---
+++ src/Components/Registration.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function Registration() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBWQuyweq6en5j5siHq5EW3y-btXks-Ty8",
+//   authDomain: "ssna-admin.firebaseapp.com",
+//   databaseURL: "https://ssna-admin-default-rtdb.firebaseio.com",
+//   projectId: "ssna-admin",
+//   storageBucket: "ssna-admin.appspot.com",
+//   messagingSenderId: "879576680646",
+//   appId: "1:879576680646:web:1ef428430e21783e1e6fc7"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
