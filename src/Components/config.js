d930cee83c4e889178b73851b7ec9fd482068fe7
Shahzaib Shafiq <73356944+shahzaibshafiq100@users.noreply.github.com>
2023-12-08 18:01:14 +0500

    Auth Added in Config File

--- src/Components/config.js
+++ src/Components/config.js
@@ -1,10 +1,6 @@
 
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+import {getAuth,GoogleAuthProvider} from 'firebase/auth'
 const firebaseConfig = {
   apiKey: "AIzaSyBWQuyweq6en5j5siHq5EW3y-btXks-Ty8",
   authDomain: "ssna-admin.firebaseapp.com",
@@ -17,3 +13,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const auth=getAuth(app);
+
+const provider=new GoogleAuthProvider();
+export {auth,provider};
