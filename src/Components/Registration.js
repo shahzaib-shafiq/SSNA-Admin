ae1850a19186e72bdb209a387c34f6010dd6e9dc
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2023-12-13 15:38:45 +0500

    Display Error Message On Screen

--- src/Components/Registration.js
+++ src/Components/Registration.js
@@ -1,81 +1,59 @@
-import React, { useEffect, useState } from 'react'
-import { auth, provider } from './config'
-import { signInWithPopup } from 'firebase/auth'
+import React, { useEffect, useState } from 'react';
+import { auth, provider } from './config';
+import { signInWithPopup } from 'firebase/auth';
 import HomePage from './HomePage';
 
 import Button from '@mui/material/Button';
-
-
-import '../Styles/Registration.css'
+import '../Styles/Registration.css';
 import GoogleIcon from '@mui/icons-material/Google';
 
 export default function Registration() {
-
   const [value, setValue] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const GoogleRegistration = () => {
-    // signInWithPopup(auth, provider).then((data) => {
-    //   setValue(data.user.email)
-    //   localStorage.setItem("email", data.user.email)
-
-
-    // })
-
     signInWithPopup(auth, provider)
-  .then((data) => {
-    const userEmail = data.user.email;
-    if (userEmail.endsWith('@cfd.nu.edu.pk')) {
-      setValue(userEmail);
-      localStorage.setItem("email", userEmail);
-    } else {
-      // Show error message for invalid email domain
-      alert('Invalid email domain. Please use an email with the domain @cfd.nu.edu.pk');
-    }
-  })
-  .catch((error) => {
-    // Handle the sign-in error here
-    console.error("Sign-in error:", error);
-  });
-
-  }
+      .then((data) => {
+        const userEmail = data.user.email;
+        if (userEmail.endsWith('@cfd.nu.edu.pk')) {
+          setValue(userEmail);
+          localStorage.setItem('email', userEmail);
+        } else {
+          setErrorMessage('Invalid email . Please use an email with the domain @cfd.nu.edu.pk');
+        }
+      })
+      .catch((error) => {
+        // Handle the sign-in error here
+        console.error('Sign-in error:', error);
+        setErrorMessage('Error during sign-in. Please try again.');
+      });
+  };
 
   useEffect(() => {
-    setValue(localStorage.getItem('email'))
-  })
+    setValue(localStorage.getItem('email'));
+  }, []);
+
   return (
     <div>
-      {value ? <HomePage /> :
-       
+      {value ? (
+        <HomePage />
+      ) : (
         <>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        
-        <br></br>
-        
-        <Button className='RegButton'  variant="contained" size="medium"
-        onClick={GoogleRegistration}
-        
-        endIcon={<GoogleIcon />}
-        >Login With Google</Button>
-        
+          <br />
+          {/* Other spacing elements */}
+          {errorMessage && <div className="error-message">{errorMessage}</div>}
+          <Button
+            className='RegButton'
+            variant="contained"
+            size="medium"
+            onClick={GoogleRegistration}
+            endIcon={<GoogleIcon />}
+          >
+            Login With Google
+          </Button>
+          {/* Other spacing elements */}
         </>
-
-
-      }
+      )}
     </div>
-  )
+  );
 }
