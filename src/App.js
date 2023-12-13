6594ceaca94760507ab2f8a7c461ab6b0ec70ae6
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2023-12-13 16:24:51 +0500

    Implemented Routing of Pages

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import Announcements from './Components/Announcements'
 import Registration from './Components/Registration';
 import HomePage from './Components/HomePage';
 import FacultyData from './Components/FacultyData';
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-  {/* <FacultyInfo/> */}
-    {/* <Announcements/> */}
-{/* <HomePage/> */}
-     <Registration/>
-{/* <FacultyData/> */}
-    
+  
+    <BrowserRouter>
+    <Routes>
+<Route path='/' element={<Registration/>}></Route>
+<Route path='/FacultyData' element={<FacultyData/>}></Route>
+<Route path='/Announcement' element={<Announcements/>}></Route>
+
+
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import Button from '@mui/material/Button';
 import '../Styles/HomePage.css';
 import LogoutIcon from '@mui/icons-material/Logout';
@@ -7,33 +6,33 @@ import SendIcon from '@mui/icons-material/Send';
 import Stack from '@mui/material/Stack';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import AnnouncementIcon from '@mui/icons-material/Announcement';
-
+import {Link} from 'react-router-dom'
 
 export default function HomePage() {
+
+
   const logout = () => {
     localStorage.clear()
     window.location.reload()
   }
+
   return (
     <div>
       <h1 className='pageheading'>Welcome Admin </h1>
 
-
       <div className="container">
 
-      <Button variant="contained" size="medium"
-       onClick={logout}
-     
-       endIcon={<PersonAddIcon />}
-     >Add Faculty</Button>
-      
+      <Link to='/FacultyData'>
+          <Button variant='contained' size='medium' endIcon={<PersonAddIcon />}>
+            Add Faculty
+          </Button>
+        </Link>
 
-      <Button variant="contained" size="medium" endIcon={<AnnouncementIcon />}
-      
-      onClick={logout}
-    
-      >Announcement</Button>
-      
+        <Link to='/announcement'>
+          <Button variant='contained' size='medium' endIcon={<AnnouncementIcon />}>
+            Announcement
+          </Button>
+        </Link>
       
       <Button variant="contained" endIcon={<LogoutIcon />}
       
