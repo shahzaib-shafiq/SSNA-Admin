7912ff7b1d1d7d3ace2697049d909257c87b0365
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2023-12-13 15:32:19 +0500

    NU Email Check Added

--- src/Components/Registration.js
+++ src/Components/Registration.js
@@ -5,6 +5,8 @@ import HomePage from './HomePage';
 
 import Button from '@mui/material/Button';
 
+
+import '../Styles/Registration.css'
 import GoogleIcon from '@mui/icons-material/Google';
 
 export default function Registration() {
@@ -12,10 +14,29 @@ export default function Registration() {
   const [value, setValue] = useState('');
 
   const GoogleRegistration = () => {
-    signInWithPopup(auth, provider).then((data) => {
-      setValue(data.user.email)
-      localStorage.setItem("email", data.user.email)
-    })
+    // signInWithPopup(auth, provider).then((data) => {
+    //   setValue(data.user.email)
+    //   localStorage.setItem("email", data.user.email)
+
+
+    // })
+
+    signInWithPopup(auth, provider)
+  .then((data) => {
+    const userEmail = data.user.email;
+    if (userEmail.endsWith('@cfd.nu.edu.pk')) {
+      setValue(userEmail);
+      localStorage.setItem("email", userEmail);
+    } else {
+      // Show error message for invalid email domain
+      alert('Invalid email domain. Please use an email with the domain @cfd.nu.edu.pk');
+    }
+  })
+  .catch((error) => {
+    // Handle the sign-in error here
+    console.error("Sign-in error:", error);
+  });
+
   }
 
   useEffect(() => {
@@ -26,8 +47,26 @@ export default function Registration() {
       {value ? <HomePage /> :
        
         <>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
         
-        <Button  variant="contained" size="medium"
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        
+        <br></br>
+        
+        <Button className='RegButton'  variant="contained" size="medium"
         onClick={GoogleRegistration}
         
         endIcon={<GoogleIcon />}
