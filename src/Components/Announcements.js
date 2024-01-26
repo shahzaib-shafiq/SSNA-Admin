2daf44802bdfe84176ad1ef006d2988b25a9ca4c
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-01-26 23:02:22 +0500

    Created All Fields Error in Uploading Data to DB

--- src/Components/Announcements.js
+++ src/Components/Announcements.js
@@ -276,22 +276,20 @@ import { Link } from 'react-router-dom'
 import Button from '@mui/material/Button';
 import HomeIcon from '@mui/icons-material/Home';
 const initialValues = {
-  name: "",
-  phone: "",
-  email: "",
-  address: "",
-  Education: "",
-  University: "",
-  Department: "",
-  areaOfIntrest: "",
-  id: ""
-
+  
+  title:"",
+  summary:"",
+  description:"",
+  AnnouncementDate:"",
 };
 
 
- const Announcements = () => {
+const Announcements = () => {
 
-
+//Function to generate a random 6-digit ID
+    const generateRandomId = () => {
+      return Math.floor(1000000 + Math.random() * 9000000).toString();
+    };
   const [img, setImg] = useState('');
   const {
     values,
@@ -304,9 +302,10 @@ const initialValues = {
     initialValues,
     validationSchema: signUpSchema,
     onSubmit: (values, action) => {
-      const { name, phone, email, address, Education, Department, University, areaOfIntrest, id } = values;
+      const { title,summary, description,AnnouncementDate } = values;
 
-      if (name && phone && email && address && Education && Department && University && areaOfIntrest, id) {
+      if (title && summary && description &&AnnouncementDate) {
+        const randomId = generateRandomId();
         const res = fetch(
           "https://ssna-admin-default-rtdb.firebaseio.com/Announcements.json",
           {
@@ -315,15 +314,11 @@ const initialValues = {
               "Content-Type": "application/json",
             },
             body: JSON.stringify({
-              name,
-              phone,
-              email,
-              address,
-              Education,
-              Department,
-              University,
-              areaOfIntrest,
-              id,
+              title,
+              summary,
+              description,
+              AnnouncementDate,
+              id:randomId,
               img: img
             }),
           }
@@ -378,17 +373,15 @@ const initialValues = {
                 <form onSubmit={handleSubmit} >
                   <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
 
-                    <div class="md:col-span-5">
-                      <label for="full_name">Enter Full Name</label>
+                      <div class="md:col-span-5">
+                      <label for="full_name">Post Title</label>
                       <input class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-
-
                         type="name"
                         autoComplete="off"
-                        name="name"
-                        id="name"
-                        placeholder="Name"
-                        value={values.name}
+                        name="title"
+                        id="title"
+                        placeholder="Title"
+                        value={values.title}
                         onChange={handleChange}
                         onBlur={handleBlur}
                       />
@@ -399,15 +392,15 @@ const initialValues = {
                     </div>
 
                     <div class="md:col-span-5">
-                      <label for="email">Email Address</label>
+                      <label for="email">Enter Summary</label>
 
                       <input class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                        placeholder="email@domain.com"
+                        placeholder="Summary"
 
-                        type="email"
+                        type="text"
                         autoComplete="off"
-                        name="email"
-                        id="email"
+                        name="summary"
+                        id=" summary"
 
                         value={values.email}
                         onChange={handleChange}
@@ -421,13 +414,13 @@ const initialValues = {
                     </div>
 
                     <div class="md:col-span-3">
-                      <label for="address">Office Adress</label>
+                      <label for="address">Description</label>
                       <input class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
                         type="text"
                         autoComplete="off"
-                        name="address"
-                        id="address"
-                        placeholder="Office Address"
+                        name="description"
+                        id="description"
+                        placeholder="Description"
                         value={values.address}
                         onChange={handleChange}
                         onBlur={handleBlur} />
@@ -448,157 +441,42 @@ const initialValues = {
                     {/*     change */}
 
                     <div class="md:col-span-2">
-                      <label for="city">Designation</label>
-                      <select
-                        name="Education"
-                        id="Education"
-                        value={values.Education}
+                      <label for="city">Announcement Date</label>
+                      <input
+                      type="date"
+                        name="AnnouncementDate"
+                        id="AnnouncementDate"
+                        value={values.address}
                         onChange={handleChange}
                         onBlur={handleBlur}
-
+                        min="2018-01-01"
+                        max="2050-12-31"
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      >
-                        {/* <option className="dropdownoption" value="">Qualification</option> */}
-                        <option className="dropdownoption" value="Professor">Professor</option>
-                        <option className="dropdownoption" value="Associate Professor">Associate Professor</option>
-                        <option className="dropdownoption" value="Assistant Professor">Assistant Professor</option>
+                      >                       
+                      </input>
 
-                        <option className="dropdownoption" value="Lecturer">Lecturer</option>
-                        <option className="dropdownoption" value=" Instructor"> Instructor</option>
-
-
-
-                      </select>
                       {errors.Education && touched.Education ? (
                         <p className="form-error">{errors.Education}</p>
                       ) : null}
 
+                   </div>
+                    
 
-
-
-                    </div>
-                    {/* Add other Fields */}
 
                     <div class="md:col-span-2">
-                      <label for="address">Department</label>
-                      <select
-                        name="Department"
-                        id="Department"
-                        value={values.Department}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Department"
-                      >
-
-                        <option value="CS">CS</option>
-                        <option value="SE">SE</option>
-                        <option value="EE">EE</option>
-                        <option value="AI">AI</option>
-                        <option value="CYS">CYS</option>
-                        <option value="BBA">BBA</option>
-                      </select>
-                      {errors.Department && touched.Department ? (
-                        <p className="form-error">{errors.Department}</p>
-                      ) : null}
-                    </div>
-
-                    <div class="md:col-span-2">
-                      <label for="state"> University</label>
-                      <input class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                        type="text"
-                        autoComplete="off"
-                        name="University"
-                        id="University"
-                        placeholder="University"
-                        value={values.University}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-
-                      />
-
-                      {errors.University && touched.University ? (
-                        <p className="form-error">{errors.University}</p>
-                      ) : null}
-                    </div>
-
-                    <div class="md:col-span-1">
-                      <label for="address">Extension</label>
+                      <label for="state">Post Image</label>
                       <input
-
-
-                        class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                        type="number"
-                        autoComplete="off"
-                        name="phone"
-                        id="phone"
-                        placeholder="Ext"
-                        value={values.phone}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                      />
-
-
-                      {errors.phone && touched.phone ? (
-                        <p className="form-error">{errors.phone}</p>
-                      ) : null}
-
-                    </div>
-
-
-
-                    <div class="md:col-span-2">
-                      <label for="country">Area of Intrest</label>
-                      <input class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                        ype="text"
-                        autoComplete="off"
-                        name="areaOfIntrest"
-                        id="areaOfIntrest"
-                        placeholder="Area of Intrest"
-                        value={values.areaOfIntrest}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-
-                      />
-                      {errors.areaOfIntrest && touched.areaOfIntrest ? (
-                        <p className="form-error">{errors.areaOfIntrest}</p>
-                      ) : null}
-                    </div>
-
-                    <div class="md:col-span-2">
-                      <label for="state">Faculty ID</label>
-                      <input class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                        type="text"
-                        autoComplete="off"
-                        name="id"
-                        id="id"
-                        placeholder="Faculty ID"
-                        value={values.id}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-
-                      />
-
-                      {errors.id && touched.id ? (
-                        <p className="form-error">{errors.id}</p>
-                      ) : null}
-                    </div>
-
-
-                    <div class="md:col-span-2">
-                      <label for="state">Faculty Image</label>
-                      <input
-                      type="file"
+                        type="file"
                         class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help" id="user_avatar" type="file"
                         onChange={(e) => handleUpload(e)}
 
                       />
 
-                 
+
                     </div>
 
-                    
-           
+
+
 
 
                     <div class="md:col-span-5 text-right">
@@ -613,7 +491,7 @@ const initialValues = {
                 </form>
 
 
-                 
+
               </div>
             </div>
           </div>
