b46bd44efa7bc86249731cae92fe373ddbeb113c
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-03-15 01:00:37 +0500

    Adding Bus Routes Page

--- src/Components/BusRoutes.js
+++ src/Components/BusRoutes.js
@@ -1,21 +1,24 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import { useFormik } from "formik";
-import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { TimetableSchema } from "../schemas";
+import { imagedb } from "./config";
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
 import { v4 } from 'uuid';
+import { Link } from 'react-router-dom'
 import ssnalogo from '../assets/ssnalogo.png';
 import homepagelogo from '../assets/homepagelogo.png';
-import { EventsSchema } from "../schemas";
-import { imagedb } from "./config";
 
 const initialValues = {
-  title: "",
-  description: "",
   EventDate: "",
 };
-
 const BusRoutes = () => {
-  const [img, setImg] = useState('');
 
+  //Function to generate a random 6-digit ID
+  const generateRandomId = () => {
+    return Math.floor(1000000 + Math.random() * 9000000).toString();
+  };
+  const [img, setImg] = useState('');
   const {
     values,
     errors,
@@ -25,48 +28,57 @@ const BusRoutes = () => {
     handleSubmit,
   } = useFormik({
     initialValues,
-    validationSchema: EventsSchema,
+    validationSchema: TimetableSchema,
     onSubmit: (values, action) => {
-      const { title, description, EventDate } = values;
+      const { EventDate } = values;
 
-      if (title && description && EventDate && img) {
-        fetch("https://ssna-admin-default-rtdb.firebaseio.com/BusRoutes.json", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            title,
-            description,
-            EventDate,
-            img: img
-          }),
-        });
+      if (EventDate) {
+        const randomId = generateRandomId();
+        const res = fetch(
+          "https://ssna-admin-default-rtdb.firebaseio.com/BusRoutes.json",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+
+              EventDate,
+              id: randomId,
+              img: img
+            }),
+          }
+        );
         action.resetForm();
       }
     },
   });
 
   const handleUpload = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      const imgs = ref(imagedb, `BusRoutes/${v4()}`)
-      uploadBytes(imgs, file).then(data => {
-        getDownloadURL(data.ref).then(val => {
-          setImg(val);
-        });
-      });
-    }
+    console.log(e.target.files[0])
+
+    const imgs = ref(imagedb, `BusRoutes/${v4()}`)
+    uploadBytes(imgs, e.target.files[0]).then(data => {
+      console.log(data, "imgs")
+      getDownloadURL(data.ref).then(val => {
+        setImg(val)
+
+      })
+    })
   }
 
   return (
-    <div className="min-h-screen p-6 bg-gray-100 flex items-center justify-center bg-blue-200">
-      <div className="container max-w-screen-lg mx-auto">
+    <div class="min-h-screen p-6 bg-gray-100 flex items-center justify-center  bg-blue-200">
+      <div class="container max-w-screen-lg mx-auto">
         <div>
-          <h2 className="font-semibold text-xl text-gray-600">Add Bus Routes</h2>
+          <h2 class="font-semibold text-xl text-gray-600">Add New Event</h2>
 
-          <div className="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
-            <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
+          <div class="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
+            <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
+
+
+
+
               <div className="text-gray-600 flex items-center">
                 <img
                   src={ssnalogo}
@@ -74,49 +86,68 @@ const BusRoutes = () => {
                   className="w-18 h-18 mr-2"
                 />
               </div>
-              <div className="lg:col-span-2">
+              <div class="lg:col-span-2">
                 <h1 className="modal-title font-sans font-bold"></h1>
                 <br></br>
 
-                <form onSubmit={handleSubmit}>
-                  <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
-                    <div className="md:col-span-2">
-                      <label htmlFor="state">Upload Bus Routes</label>
+                <form onSubmit={handleSubmit} >
+                  <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
+                    <div class="md:col-span-2">
+                      <label for="city">Upload Date</label>
+                      <input
+                        type="date"
+                        name="EventDate"
+                        id="EventDate"
+                        value={values.EventDate}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        min="2018-01-01"
+                        max="2050-12-31"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      >
+                      </input>
+                      {errors.EventDate && touched.EventDate ? (
+                        <p className="form-error">{errors.EventDate}</p>
+                      ) : null}
+
+                    </div>
+
+                    <div class="md:col-span-2">
+                      <label for="state">Upload CSV/XLSX File</label>
                       <input
                         type="file"
-                        className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                        class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
                         aria-describedby="user_avatar_help"
                         id="user_avatar"
-                        accept="application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                        accept=".pdf"
                         onChange={(e) => handleUpload(e)}
                       />
                     </div>
-                    <div className="md:col-span-5 text-right">
-                      <div className="inline-flex items-end">
+                    <div class="md:col-span-5 text-right">
+                      <div class="inline-flex items-end">
                         <button
                           type="submit"
-                          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                        >
-                          Upload Routes
-                        </button>
+                          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Upload Bus Routes</button>
                       </div>
                     </div>
+
                   </div>
                 </form>
               </div>
             </div>
           </div>
         </div>
-        <a href="/Homepage" className="md:absolute bottom-0 right-0 p-4 float-right">
-          <img
-            src={homepagelogo}
-            alt="Buy Me A Coffee"
-            className="transition-all rounded-full w-14  hover:shadow-sm shadow-lg ring hover:ring-4 ring-white"
-          ></img>
+
+        <a href="/Homepage" class="md:absolute bottom-0 right-0 p-4 float-right">
+          <img src={homepagelogo} alt="Buy Me A Coffee" class="transition-all rounded-full w-14  hover:shadow-sm shadow-lg ring hover:ring-4 ring-white"></img>
         </a>
+
       </div>
     </div>
   );
-};
+}
+
+
+
 
 export default BusRoutes;
--- src/schemas/index.jsx
+++ src/schemas/index.jsx
@@ -105,3 +105,15 @@ export const EventsSchema = Yup.object({
 
 
 });
+
+
+export const TimetableSchema = Yup.object({
+  EventDate: Yup.string()
+  .matches(
+    /^(19[0-9][0-9]|20[0-4][0-9]|2050)-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/,
+    "Invalid date format. Use YYYY-MM-DD format."
+  )
+  .required("Enter Post Date"),
+
+
+});
